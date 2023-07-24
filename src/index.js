09119ad8642e0194a78c4737773a8c6a3a223201
María Sánchez <msanchezroman95@gmail.com>
2023-07-24 09:38:41 +0200

    add endpoints

--- src/index.js
+++ src/index.js
@@ -30,3 +30,95 @@ server.listen(port, () => {
 });
 
 // Endpoints
+
+// get all recipes
+server.get('/api/recetas', async (req, res) => {
+  const select = 'SELECT * FROM recetas';
+  const conn = await getConnection();
+  const [result] = await conn.query(select);
+  const numOfElements = result.length;
+  conn.end();
+  res.json({
+    info: { count: numOfElements },
+    results: result,
+  });
+});
+
+// get recipe by id
+server.get('/api/recetas/:id', async (req, res) => {
+  const recipeId = req.params.id;
+  const select = 'SELECT * FROM recetas WHERE id = ?';
+  const conn = await getConnection();
+  const [result] = await conn.query(select, [recipeId]);
+  conn.end();
+  res.json({
+    results: result,
+  });
+});
+
+// add new recipe
+server.post('/api/recetas', async (req, res) => {
+  const { nombre, ingredientes, instrucciones } = req.body;
+  try {
+    if (nombre !== '' && ingredientes !== '' && instrucciones !== '') {
+      const insert = 'INSERT INTO recetas (nombre, ingredientes, instrucciones) VALUES(?,?,?)';
+      const conn = await getConnection();
+      const [result] = await conn.query(insert, [nombre, ingredientes, instrucciones]);
+      conn.end();
+      res.json({
+        success: true,
+        id: result.insertId,
+      });
+    } else {
+      res.json({
+        success: false,
+        message: 'Faltan campos por rellenar',
+      });
+    }
+  } catch (e) {
+    res.json({
+      success: false,
+      message: `Hubo un error intentando crear la receta: ${e}`,
+    });
+  }
+});
+
+// update recipe
+server.put('/api/recetas/:id', async (req, res) => {
+  const recipeId = req.params.id;
+  const { nombre, ingredientes, instrucciones } = req.body;
+  try {
+    const updateRecipe = 'update recetas SET nombre = ?, ingredientes = ?, instrucciones = ? WHERE id = ?';
+    const conn = await getConnection();
+    const [result] = await conn.query(updateRecipe, [nombre, ingredientes, instrucciones, recipeId]);
+    res.json({
+      success: true,
+      message: `Receta actualizada correctamente`,
+    });
+  } catch (e) {
+    res.json({
+      success: false,
+      message: `Hubo un error modificando la receta: ${e}`,
+    });
+  }
+});
+
+// delete recipe
+server.delete('/api/recetas/:id', async (req, res) => {
+  const recipeId = req.params.id;
+  try {
+    const deleteQ = 'DELETE FROM recetas WHERE id = ?';
+    const conn = await getConnection();
+    const [result] = await conn.query(deleteQ, [recipeId]);
+    conn.end();
+    res.json({
+      success: true,
+      message: 'Receta eliminada correctamente',
+    });
+  } catch (e) {
+    res.json({
+      success: false,
+      message: 'Ha ocurrido un error',
+    });
+  }
+});
